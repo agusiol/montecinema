d199584472de0b536d12477198362d6a2a3f518f
Agnieszka Rohm <agnieszka.rohm@gmail.com>
2021-05-27 16:45:37 +0200

    BaseRepository added

---
+++ app/concepts/base/base_repository.rb
@@ -0,0 +1,29 @@
+module Base
+  class BaseRepository
+    attr_reader :adapter
+
+    def initialize(adapter: )
+      @adapter = adapter
+    end
+
+    def find_all
+      adapter.order(:id).all
+    end
+
+    def find_by(id)
+      adapter.find(id)
+    end
+
+    def create(params)
+      adapter.create(params)
+    end
+
+    def update(id, params)
+      adapter.update(id, params)
+    end
+
+    def delete(id)
+      adapter.destroy(id)
+    end
+  end
+end
--- app/concepts/cinema_halls/repository.rb
+++ app/concepts/cinema_halls/repository.rb
@@ -1,32 +1,8 @@
 module CinemaHalls
-  class Repository
-    attr_reader :adapter
+  class Repository < ::Base::BaseRepository
 
     def initialize(adapter: CinemaHall)
       @adapter = adapter
-    end
-
-    # def fetch_all(columns:)
-    #   adapter.select(*columns)
-    # end
-    def find_all
-      adapter.order(:id).all
-    end
-
-    def find_by(id)
-      adapter.find(id)
-    end
-
-    def create(params)
-      adapter.create(params)
-    end
-
-    def update(id, params)
-      adapter.update(id, params)
-    end
-
-    def delete(id)
-      adapter.destroy(id)
     end
   end
 end
