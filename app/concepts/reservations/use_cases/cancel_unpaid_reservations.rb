7772f9f354c127adbccbd8801c5d435808626dfa
Agnieszka Rohm <agnieszka.rohm@gmail.com>
2021-06-08 10:44:39 +0200

    Bug fixed

--- app/concepts/reservations/use_cases/cancel_unpaid_reservations.rb
+++ app/concepts/reservations/use_cases/cancel_unpaid_reservations.rb
@@ -5,17 +5,15 @@ module Reservations
 
       def initialize(repository: Reservations::Repository.new)
         @repository = repository
+      end
 
       def call(screening_id)
           reservations = @repository.unpaid(screening_id)
           reservations.map do |reservation|
             client = Clients::Repository.new.find_by(reservation.client_id)
             ReservationMailer.with(reservation: reservation, email: client.email).cancellation_email.deliver_later
-
             Reservations::UseCases::Delete.new.call(id: reservation.id)
-
           end
-        end
       end
     end
   end
--- spec/jobs/cancel_reservations_job_spec.rb
+++ spec/jobs/cancel_reservations_job_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe CancelReservationsJob, type: :job do
   let(:cinema_hall) {create(:cinema_hall)}
   let(:movie) {create(:movie)}
-  let(:screening) {create(:screening, date: Time.current - 30.minutes, movie: movie, cinema_hall: cinema_hall) }
+  let(:screening) {create(:screening, date: Time.current + 20.minutes, movie: movie, cinema_hall: cinema_hall) }
   let(:cancel_unpaid_reservations) { instance_double(Reservations::UseCases::CancelUnpaidReservations, call: spy) }
   before do
     allow(Reservations::UseCases::CancelUnpaidReservations).to receive(:new).and_return(cancel_unpaid_reservations)
