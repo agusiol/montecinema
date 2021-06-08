18bdee1887db61f9d16f05dbebbbc127de161e0b
Agnieszka Rohm <agnieszka.rohm@gmail.com>
2021-06-08 09:50:29 +0200

    job test changed

--- spec/jobs/cancel_reservations_job_spec.rb
+++ spec/jobs/cancel_reservations_job_spec.rb
@@ -4,10 +4,13 @@ RSpec.describe CancelReservationsJob, type: :job do
   let(:cinema_hall) {create(:cinema_hall)}
   let(:movie) {create(:movie)}
   let(:screening) {create(:screening, date: Time.current - 30.minutes, movie: movie, cinema_hall: cinema_hall) }
-  let(:cancel_unpaid_reservations) { instance_double(Reservations::UseCases::CancelUnpaidReservations, call: true) }
+  let(:cancel_unpaid_reservations) { instance_double(Reservations::UseCases::CancelUnpaidReservations) }
+  before do
+    allow(Reservations::UseCases::CancelUnpaidReservations).to receive(:new).and_return(cancel_unpaid_reservations)
+    end
 
   it "runs CancelUnpaidReservations use case" do
-    expect(Reservations::UseCases::CancelUnpaidReservations).to receive(:new).and_return(cancel_unpaid_reservations)
+ 
     expect(cancel_unpaid_reservations).to receive(:call).with(screening.id)
   end
 end
