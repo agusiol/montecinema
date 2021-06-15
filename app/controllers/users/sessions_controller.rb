a2193813824b2928ea7b90bab43fe0b47a22a879
Agnieszka Rohm <agnieszka.rohm@gmail.com>
2021-06-15 13:19:10 +0200

    fixed

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -7,23 +7,17 @@ class Users::SessionsController < Devise::SessionsController
   private
 
   def respond_with(resource, _opts = {})
-    render json: { message: 'You are logged in.' }, status: :ok
+    if resource.errors.empty?
+      render json: { message: "You are logged in." }, status: :ok
+    else
+      render json: resource.errors
+    end
+  
   end
 
   def respond_to_on_destroy
-    log_out_success && return if current_user
-
-    log_out_failure
-  end
-
-  def log_out_success
     render json: { message: "You are logged out." }, status: :ok
   end
-
-  def log_out_failure
-    render json: { message: "Hmm nothing happened."}, status: :unauthorized
-  end
-
 
   # GET /resource/sign_in
   # def new
