caa28da4dbc9337d9f663cd5476f79e626db7ca0
Agnieszka Rohm <42643953+agusiol@users.noreply.github.com>
2021-06-16 12:05:07 +0200

    Pundit install (#24)
    * Pundit installed
    
    * Add role to Users
    
    * CinemaHall Policy

--- Gemfile
+++ Gemfile
@@ -57,5 +57,6 @@ end
 
 gem 'devise'
 gem 'devise-jwt'
+gem "pundit"
 gem 'rubocop', require: false
 gem 'simplecov', require: false
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+
+  enum role: { client: 0, employee: 1, admin: 2 }
 end
