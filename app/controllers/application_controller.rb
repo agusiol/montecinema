14e70fa1e12cbc09d77f1d989214bdae248919bc
Agnieszka Rohm <agnieszka.rohm@gmail.com>
2021-06-16 09:54:06 +0200

    Add role to Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::API
   respond_to :json
   include ActionController::MimeResponds
   include Pundit
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+  end
 end
