ad5537949f1923fc1535e58713b33cf2c3e8f5c7
Agnieszka Rohm <agnieszka.rohm@gmail.com>
2021-06-21 12:35:17 +0200

    Offline endpoint doc

--- app/controllers/offline/reservations_controller.rb
+++ app/controllers/offline/reservations_controller.rb
@@ -5,16 +5,39 @@ module Offline
     before_action :authenticate_user!
     before_action :staff_authorize!
 
+    resource_description do
+      short 'Offline Reservations/ Only for staff members'
+      formats ['json']
+    end
+
+    def_param_group :reservation do
+      param :reservation, Hash do
+        param :screening_id , Integer, required: true
+        param :ticket_desk_id , Integer, required: true
+        param :status , String, 'confirmed or paid', required: true
+        param :tickets, Array, ' Array of tickets with {price ticket_type seat}', required: true do
+          param :price, Float, required: true
+          param :seat, String , 'ex. "1A" , 2E', required: true
+          param :ticket_type, String, required: true
+        end
+      end
+    end
+
+    api :GET, "/offline/reservations", "Display all reservations "
+
     def index
       @reservations = Reservations::Repository.new.find_all
       render json: Reservations::Representer.new(@reservations).basic
     end
 
+    api :GET, "/offline/reservations/:id", "Detailed info about reservation"
     def show
       reservation = Reservations::Repository.new.find_by(params[:id])
       render json: Reservations::Representer.new([reservation]).extended
     end
 
+    api :POST, "/offline/reservations", "Creates a reservation with tickets"
+    param_group :reservation
     def create
       user = Users::Repository.new.offline_user
       @reservation = Reservations::UseCases::CreateOffline.new.call(params: reservation_params.merge(user_id: user.id))
@@ -23,6 +46,7 @@ module Offline
       render json: { error: e.message }.to_json
     end
 
+    api :PUT, "/offline/reservations/:id", "Changes reservation status from confirmed to paid "
     def update
       reservation = Reservations::UseCases::Pay.new.call(id: params[:id])
       if reservation.valid?
@@ -32,6 +56,7 @@ module Offline
       end
     end
 
+    api :DELETE, "/offline/reservations/:id", "Deletes reservation "
     def destroy
       Reservations::UseCases::Delete.new.call(id: params[:id])
       render json: { status: 'deleted' }
