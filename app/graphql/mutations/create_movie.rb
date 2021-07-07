743f02fefb9da04d40957d797a5281fee7a9001f
Agnieszka Rohm <agnieszka.rohm@gmail.com>
2021-07-07 11:19:36 +0200

    Create Movie mutation

---
+++ app/graphql/mutations/create_movie.rb
@@ -0,0 +1,28 @@
+module Mutations
+  class CreateMovie < BaseMutation
+    argument :title, String, required: true
+    argument :genre, String, required: true
+    argument :age_allowed, Integer, required: true
+    argument :duration, Integer, required: true
+
+    field :movie, Types::MovieType, null: true
+    field :errors, [String], null: false
+
+    def resolve(**attributes)
+      movie = Movies::UseCases::Create.new.call(params: attributes)
+      
+      if movie.valid?
+        {
+          movie: movie,
+          errors: [],
+        }
+      else
+        {
+          movie: nil,
+          errors: comment.errors.full_messages
+        }
+      end
+      # {status: 200, movie: movie}
+    end
+  end
+end
